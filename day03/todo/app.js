4598f17cf429b43a26c739c906e77ac7fdb4519d
kimjunseok777 <wnszn5183@naver.com>
2024-07-27 23:30:54 +0900

    node

--- day03/todo/app.js
+++ day03/todo/app.js
@@ -6,14 +6,19 @@ import User from "./routes/user.js"
 
 const app = express()
 db.sequelize.sync().then(() => {
+    // sequelize : 객체와 관계형 DB를 맵핑
     console.log('연결 성공')
 })
 
 app.use(express.json()) //-->  json 사용해서 쓴다는 의미
 app.use(cookieParser()) //-->  cookieParser 사용한다는 의미
 
-app.use("/user", User) //-->  주소 기본값과, User 를 등록시켜준 것이다
+//----------------------------------------------------------------------------------------------
 
+app.use("/user", User) //-->  주소 기본값과, User 를 등록시켜준 것이다
+// User 는 회원가입 요직이 있는 router 를 import 받아준 것이다  -->  메소드 : post / 주소 : "/sign-up"
+
+//----------------------------------------------------------------------------------------------
 
 app.listen(3040, () => {
     console.log("start server port : 3040")
--- day03/todo/routes/user.js
+++ day03/todo/routes/user.js
@@ -11,13 +11,13 @@ router.post("/sign-up", async (req, res) => {
         where: {
             email
         }
-    }
-)
+    })
+
     if(existUser) return res.json({
         ok: false,
         message: "이미 존재하는 이메일입니다"
-    }
-)
+    })
+
     const hashedPassword = await bcrypt.hash(password, 12)
     //-->  여기서 12 는 몇번 솔트할건지이다 (솔트가 크면 클 수록 암호화를 더 많이 해서 보안이 높아지는 대신에, 서버의 부담이 커진다)
     await Users.create({ //-->  여기는 insert 랑 똑같다
@@ -29,6 +29,7 @@ router.post("/sign-up", async (req, res) => {
         ok: true,
         message: "회원가입을 축하합니다"
     })
+
 })
 
 export default router
